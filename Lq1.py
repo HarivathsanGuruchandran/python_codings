7d75864eba062adc0f1f8a227ed570ab16186a5c
HarivathsanGuruchandran <133837019+HarivathsanGuruchandran@users.noreply.github.com>
2023-06-07 12:22:15 +0530

    Add files via upload

---
+++ Lq1.py
@@ -0,0 +1,9 @@
+ec=0
+oc=0
+for i in range (20,50):
+    if(i%2==0):
+        ec=ec+1
+    else:
+        oc=oc+1
+print("THE TOTAL NUMBER OF ODD NUMBERS IN THE RANGE:",oc)
+print("THE TOTAL NUMBER OF EVEN NUMBERS IN THE RANGE:",ec) 
